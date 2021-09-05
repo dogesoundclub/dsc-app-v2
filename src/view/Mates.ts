b97cffe156767f05fa4e0a8764714ac5b3e12667
Hanul <hanul@hanul.me>
2021-09-05 21:59:47 +0900

    메이트 소개 뷰 개발

--- src/view/Mates.ts
+++ src/view/Mates.ts
@@ -10,6 +10,25 @@ export default class Mates implements View {
     constructor() {
         Layout.current.title = msg("MATES_TITLE");
         Layout.current.content.append(this.container = el(".mates-view",
+            el("h1", msg("MATES_TITLE")),
+            el("section",
+                el("h2", msg("MATES_SECTION_1_TITLE")),
+                el("p", msg("MATES_SECTION_1")),
+            ),
+            el("section",
+                el("h2", msg("MATES_SECTION_2_TITLE")),
+                el("p", msg("MATES_SECTION_2")),
+            ),
+            el("section",
+                el("h2", msg("MATES_SECTION_3_TITLE")),
+                el("a.all", { href: "https://ipfs.io/ipfs/QmfTimyAQTQjQsnvECn9U44LdnPzSDF2XREoP2WFdjHitQ?filename=dscMates.png", target: "_blank" },
+                    el("img", { src: "https://ipfs.io/ipfs/QmfTimyAQTQjQsnvECn9U44LdnPzSDF2XREoP2WFdjHitQ?filename=dscMates.png" }),
+                ),
+                el("p", msg("MATES_SECTION_3")),
+                el("a", { href: "https://ipfs.io/ipfs/QmfTimyAQTQjQsnvECn9U44LdnPzSDF2XREoP2WFdjHitQ?filename=dscMates.png", target: "_blank" },
+                    "https://ipfs.io/ipfs/QmfTimyAQTQjQsnvECn9U44LdnPzSDF2XREoP2WFdjHitQ?filename=dscMates.png",
+                ),
+            ),
         ));
     }
 
