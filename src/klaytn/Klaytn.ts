4da1109e70255f109ad2959406ae1cff7747d992
Hanul <hanul@hanul.me>
2021-11-19 21:54:23 +0900

    wws

--- src/klaytn/Klaytn.ts
+++ src/klaytn/Klaytn.ts
@@ -1,17 +1,20 @@
 class Klaytn {
 
-    private caver = new (window as any).Caver(new (window as any).Caver.providers.WebsocketProvider("wss://klaytn-node.klu.bs:9091"));
+    private caver = new (window as any).Caver(new (window as any).Caver.providers.WebsocketProvider("wss://klaytn-node.klu.bs:9091", {
+        reconnect: {
+            auto: true,
+            delay: 1000,
+            maxAttempts: true,
+            onTimeout: false
+        },
+    }));
 
     public createContract(address: string, abi: any) {
-        return this.caver === undefined ?
-            new (window as any).caver.klay.Contract(abi, address) :
-            this.caver.contract.create(abi, address);
+        return this.caver.contract.create(abi, address);
     }
 
     public async loadBlockNumber() {
-        return this.caver === undefined ?
-            await (window as any).caver.klay.getBlockNumber() :
-            await this.caver.klay.getBlockNumber();
+        return await this.caver.klay.getBlockNumber();
     }
 }
 
